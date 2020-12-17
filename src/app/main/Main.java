95c9a414082ef9c82d18140827ca3c7736306411
cinekele <56127100+cinekele@users.noreply.github.com>
2020-12-17 21:10:35 +0100

    initial map

--- src/app/main/Main.java
+++ src/app/main/Main.java
@@ -1,8 +1,38 @@
 package app.main;
 
-public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+import app.main.map.SVGMap;
+
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+
+/**
+ * A simple sample application that shows
+ * a OSM map of World
+ * @author Martin Steiger
+ */
+public class Main
+{
+    /**
+     * @param args the program args (ignored)
+     */
+    public static void main(String[] args) throws IOException {
+        // Create a new JFrame.
+        JFrame frame = new JFrame("Batik");
+
+        frame.setSize(800, 800);
+        // Add components to the frame.
+        frame.getContentPane().add(new SVGMap().getSvgCanvas());
+
+
+        // Display the frame.
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        frame.setVisible(true);
     }
 }
---
+++ src/app/main/map/SVGMap.java
@@ -0,0 +1,58 @@
+package app.main.map;
+
+import org.apache.batik.anim.dom.SAXSVGDocumentFactory;
+import org.apache.batik.swing.JSVGCanvas;
+import org.apache.batik.util.XMLResourceDescriptor;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.svg.SVGDocument;
+
+import java.io.IOException;
+
+/**
+ * Class that generates a world map
+ * with series of data on her.
+ */
+public class SVGMap {
+    private static final String path = "world.svg";
+    SVGDocument svgDocument;
+    JSVGCanvas jsvgCanvas = new JSVGCanvas();
+
+
+    public SVGMap() {
+        String parser = XMLResourceDescriptor.getXMLParserClassName();
+        SAXSVGDocumentFactory f = new SAXSVGDocumentFactory(parser);
+        try {
+            svgDocument = (SVGDocument) f.createDocument(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void colorMap(String country, String color){
+        NodeList nodeList = svgDocument.getElementsByTagName("path");
+        for (int i = 0; i < nodeList.getLength(); i++){
+            Element element = (Element) nodeList.item(i);
+            if (element.getAttribute("id").equalsIgnoreCase(country))
+                if (element.hasAttribute("fill")) element.getAttributeNode("fill").setValue(color);
+                else element.setAttribute("fill", color); //tworzenie atrybutu
+        }
+    }
+
+
+    /**
+     * @return object, which can be displayed using Swing.
+     */
+    public JSVGCanvas getSvgCanvas() {
+        colorMap("PL", "blue");
+        colorMap("GB", "yellow");
+        colorMap("DK", "green");
+        colorMap("PL", "red");
+        colorMap("US", "purple");
+        colorMap("CA", "#E6E6FA");
+        jsvgCanvas.setSVGDocument(svgDocument);
+        jsvgCanvas.setDocumentState(JSVGCanvas.ALWAYS_INTERACTIVE);
+        jsvgCanvas.setEnableRotateInteractor(false);
+        return jsvgCanvas;
+    }
+}
