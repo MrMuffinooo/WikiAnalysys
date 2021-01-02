69ec4fa44818a7b5e94c05e00353ea4ec7e40e22
CiNeKele <01151428@pw.edu.pl>
2021-01-03 00:10:09 +0100

    Backup modyfikowanie mapy

--- src/app/main/map/MapData.java
+++ src/app/main/map/MapData.java
@@ -12,7 +12,7 @@ public class MapData {
     }
 
     private void prepareColors(){
-        ColorPallete pallete = new ColorPallete(data.stream().map(MapRecord::getNumber_of_exposures).collect(Collectors.toList()));
+        ColorPallete pallete = new ColorPallete(data.stream().map(MapRecord::getNumberOfExposures).collect(Collectors.toList()));
         List<String> rgbCodes = pallete.getRGBcodes();
         for (int i = 0; i < rgbCodes.size(); i++){
             String color =  rgbCodes.get(i);
@@ -27,14 +27,14 @@ public class MapData {
 
 class MapRecord {
     private String id;
-    private String country_name;
-    private Integer number_of_exposures;
+    private String countryName;
+    private Integer numberOfExposures;
     private String rgbColor;
 
-    public MapRecord(String id, String country_name, Integer number_of_exposures) {
+    public MapRecord(String id, String countryName, Integer numberOfExposures) {
         this.id = id;
-        this.country_name = country_name;
-        this.number_of_exposures = number_of_exposures;
+        this.countryName = countryName;
+        this.numberOfExposures = numberOfExposures;
     }
 
     public void setRGBColor(String rgbColor) {
@@ -49,12 +49,12 @@ class MapRecord {
         return id;
     }
 
-    public String getCountry_name() {
-        return country_name;
+    public String getCountryName() {
+        return countryName;
     }
 
-    public Integer getNumber_of_exposures() {
-        return number_of_exposures;
+    public Integer getNumberOfExposures() {
+        return numberOfExposures;
     }
 
     @Override
@@ -62,11 +62,11 @@ class MapRecord {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         MapRecord mapRecord = (MapRecord) o;
-        return Objects.equals(id, mapRecord.id) && Objects.equals(country_name, mapRecord.country_name) && Objects.equals(number_of_exposures, mapRecord.number_of_exposures) && Objects.equals(rgbColor, mapRecord.rgbColor);
+        return Objects.equals(id, mapRecord.id) && Objects.equals(countryName, mapRecord.countryName) && Objects.equals(numberOfExposures, mapRecord.numberOfExposures) && Objects.equals(rgbColor, mapRecord.rgbColor);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, country_name, number_of_exposures, rgbColor);
+        return Objects.hash(id, countryName, numberOfExposures, rgbColor);
     }
 }
--- src/app/main/map/SVGMap.java
+++ src/app/main/map/SVGMap.java
@@ -4,15 +4,18 @@ import org.apache.batik.anim.dom.SAXSVGDocumentFactory;
 import org.apache.batik.anim.dom.SVGDOMImplementation;
 import org.apache.batik.bridge.*;
 import org.apache.batik.gvt.GraphicsNode;
+import org.apache.batik.svggen.SVGGraphics2D;
+import org.apache.batik.svggen.SVGGraphics2DIOException;
 import org.apache.batik.swing.JSVGCanvas;
 import org.apache.batik.util.XMLResourceDescriptor;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.w3c.dom.svg.SVGDocument;
 import org.w3c.dom.svg.SVGElement;
 import org.w3c.dom.svg.SVGLocatable;
 import org.w3c.dom.svg.SVGRect;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 
 /**
@@ -68,11 +71,12 @@ public class SVGMap {
         SVGRect rect = locatable.getBBox(); //znalezienie boxa sciezki
 
         Element text = svgDocument.createElementNS(SVGDOMImplementation.SVG_NAMESPACE_URI,"text");
-        text.setAttribute("fill", "red");
+        text.setAttribute("id",country.getId() + "T");
+        text.setAttribute("fill", "blue");
         text.setAttribute("font-size", "5px");
         text.setAttribute("x", String.valueOf(rect.getX() +rect.getWidth()/2));
         text.setAttribute("y", String.valueOf(rect.getY() + rect.getHeight()/2));
-        text.setTextContent(country.getNumber_of_exposures().toString());
+        text.setTextContent(country.getNumberOfExposures().toString());
         root.appendChild(text);
     }
 
@@ -83,12 +87,46 @@ public class SVGMap {
         }
     }
 
+    private void saveMap(){
+        try (OutputStreamWriter out = new OutputStreamWriter(new FileOutputStream("new world.svg"), "UTF-8")){
+            NodeList nodeList = svgDocument.getElementsByTagNameNS(SVGDOMImplementation.SVG_NAMESPACE_URI, "path");
+            Element root = svgDocument.getRootElement();
+            for (int i = 0; i < nodeList.getLength(); i++){
+                Element element = (Element) nodeList.item(i);
+                element.setAttribute("fill", "black");
+
+                SVGLocatable locatable = (SVGLocatable) element;
+                SVGRect rect = locatable.getBBox(); //znalezienie boxa sciezki
+
+                Element text = svgDocument.createElementNS(SVGDOMImplementation.SVG_NAMESPACE_URI,"text");
+                text.setAttribute("id",element.getAttribute("id") + "T");
+                text.setAttribute("fill", "blue");
+                text.setAttribute("font-size", "5px");
+                text.setAttribute("title", element.getAttribute("title"));
+                text.setAttribute("x", String.valueOf(rect.getX() +rect.getWidth()*0.4));
+                text.setAttribute("y", String.valueOf(rect.getY() + rect.getHeight()*0.4));
+                text.setTextContent("");
+                root.appendChild(text);
+            }
+            SVGGraphics2D graphics2D = new SVGGraphics2D(svgDocument);
+            graphics2D.stream(svgDocument.getDocumentElement(),out, true, true);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (SVGGraphics2DIOException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     /**
      * @return object, which can be displayed using Swing.
      */
     public JSVGCanvas getSvgCanvas() {
+        saveMap();
         jsvgCanvas.setDocumentState(JSVGCanvas.ALWAYS_INTERACTIVE);
-        prepareMap();
         jsvgCanvas.setSVGDocument(svgDocument);
         jsvgCanvas.setEnableRotateInteractor(false);
         return jsvgCanvas;
